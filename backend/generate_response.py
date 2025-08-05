cfc90e63b6771e66c80975c7a3cee12e6f5a7871
Max <boltmax@hotmail.com>
2025-08-05 11:49:17 +0100

    server arg BACKEND_MODE allows for either an external API call, or custom model

--- backend/generate_response.py
+++ backend/generate_response.py
@@ -1,17 +1,21 @@
 from dotenv import load_dotenv
+load_dotenv(".env.local")
 import os
 import google.generativeai as genai
 
 
-# setup AI model
-system_prompt = "return a clever comeback to the users roast:"
-load_dotenv(".env.local")
-genai.configure(api_key= os.getenv("API_KEY"))
-model = genai.GenerativeModel(system_instruction= system_prompt)
+# if in lite mode, setup api call
+BACKEND_MODE = os.getenv("BACKEND_MODE", "normal")
+if BACKEND_MODE == "lite":
+    system_prompt = "return a clever comeback to the users roast:"
+    genai.configure(api_key= os.getenv("API_KEY"))
+    model = genai.GenerativeModel(system_instruction= system_prompt)
+
+
+def generate_lite(roast:str) -> str:
+    output = model.generate_content(roast).text
+    return output
 
 
 def generate(roast:str) -> str:
-
-    # TODO improve using modern methods
-    output = model.generate_content(roast).text
-    return output
+    return "TODO: make custom model for normal mode"
--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from generate_response import generate
+from generate_response import generate, generate_lite
 import os
+from dotenv import load_dotenv
+load_dotenv(".env.local")
 
+# set mode of server
+BACKEND_MODE = os.getenv("BACKEND_MODE", "normal")
+generate_call = generate_lite if BACKEND_MODE == "lite" else generate
 
 server = Flask(__name__)
 CORS(server)
@@ -10,9 +15,10 @@ CORS(server)
 @server.route('/comeback', methods=["POST"])
 def handle_comeback():
     
-    comeback = generate(request.get_json()['roast'])
+    comeback = generate_call(request.get_json()['roast'])
 
     return jsonify({'comeback': comeback})
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    print(f"\nStarted server in {BACKEND_MODE} mode\n")
+    server.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))  
